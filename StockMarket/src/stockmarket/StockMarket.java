dfe587eea832ece0b9bb5bbcd7cf0d6ad71bcb04
Ben Higgott <ben.higgott@hotmail.co.uk>
2015-10-26 11:53:32 +0000

    Writing to server

--- StockMarket/src/stockmarket/StockMarket.java
+++ StockMarket/src/stockmarket/StockMarket.java
@@ -7,28 +7,65 @@ package stockmarket;
 import java.net.Socket;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 /**
  *
  * @author Michael
  */
 public class StockMarket {
 
-    Socket clientSocket;
+    static Socket clientSocket;
+    static PrintWriter out;
+    static BufferedReader in;
+    static BufferedReader stdin;
     /**
      * @param args the command line arguments
      */
-    public void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {
+        System.out.println("Server Connect "+Connect());
+ 
+        //Make this methods
         try{
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            out.println("HELO");
+            System.out.println("echo: " + in.read());
+        }
+        catch(Exception e)
+        {
+            
+        }
+        
+        
+        System.out.println("Server Disconnect "+Disconnect());
+    }
+    public static boolean Connect(){
+         try{
             //Connect to the server through the socket
             clientSocket = new Socket("192.168.0.48", 5000);
-            
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-            out.printf("hello");
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.print("SquareOne connected");
         }
         catch (Exception e){
             System.out.println(e.toString());
+            return false;
         }
+        return true; 
     }
     
+    public static boolean Disconnect()
+    {
+        try{
+            in.close();
+            out.close();
+            stdin.close();
+            clientSocket.close();
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            return false;
+        }
+        return true; 
+    }
 }
